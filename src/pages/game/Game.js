45baa649b67dc51d822feac72b1ebd083b987116
Timur <tmdautov@gmail.com>
2022-05-22 21:20:55 +0600

    feat(): add filters by category and search

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,8 +1,37 @@
+import { useEffect } from 'react';
 import styles from './Game.css'; 
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 function Game() {
+  let { id } = useParams();
+  console.log(id)
+  useEffect(() => {
+    window.comeon.game.launch(id);
+
+    
+    // const { match } = this.props;
+    // console.log(match)
+  })
   return(
-    <header className={styles.error}>Game</header>
+    <div className="ingame">
+        <div className="ui grid centered">
+          <div className="three wide column">
+            <Link to="/games" className="ui right floated secondary button inverted">
+              <i className="left chevron icon"></i>Back
+            </Link>
+          </div>
+          <div className="ten wide column">
+            <div id="game-launch">
+              {/* {error && (
+                <h2 className="game__not-found">Game not found</h2>
+              )} */}
+            </div>
+          </div>
+          <div className="three wide column"></div>
+        </div>
+      </div>
   );
 }
 
--- src/pages/games/Games.js
+++ src/pages/games/Games.js
@@ -1,99 +1,124 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import useAuth from '../../hooks/useAuth';
-import styles from './Games.css'; 
+import styles from './Games.css';
 import { Link } from 'react-router-dom';
 
 function Games() {
   const { auth, setAuth } = useAuth();
   let [games, setGames] = useState([]);
+  let [filteredGames, setFilteredGames] = useState([]);
   let [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3001/games")
-    	.then(response => response.json())
-    	.then(data => {
-        console.log(data)
-        setGames(data)
+    fetch('http://localhost:3001/games')
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
       });
-  },[]);
-
+  }, []);
 
   useEffect(() => {
-    fetch("http://localhost:3001/categories")
-    	.then(response => response.json())
-    	.then(data => {
-        console.log(data)
-        // setGames(data)
-        setCategories(data)
+    fetch('http://localhost:3001/categories')
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
       });
-  },[])
+  }, []);
 
   function logout() {
-    // console.log('logout')
     setAuth(null);
   }
 
-  return(
-    <div className="casino">
-      
-      <section className="ui grid centered">
-          <div className="twelve wide column">
-          <div className="ui list">
-            <div className="player item">
-              <img className="ui avatar image" src={`../../${auth.data.avatar}`} alt="avatar" />
-              <div className="content">
-                <div className="header"><b className="name">{auth.data.name}</b></div>
-                <div className="description event">{auth.data.event}</div>
+  function changeCategories(id) {
+    console.log(id);
+
+    filteredGames = games.filter((game) => {
+      return game.categoryIds.includes(id);
+    });
+    setFilteredGames(filteredGames);
+  }
+
+  function handleSearch(evt) {
+    const searchValue = evt.target.value;
+    filteredGames = games.filter((game) => {
+      return game.name.toLowerCase().includes(searchValue);
+    });
+    setFilteredGames(filteredGames);
+  }
+
+  return (
+    <div className='casino'>
+      <section className='ui grid'>
+        <div className='twelve wide column'>
+          <div className='ui list'>
+            <div className='player item'>
+              <img className='ui avatar image' src={`../../${auth.data.avatar}`} alt='avatar' />
+              <div className='content'>
+                <div className='header'>
+                  <b className='name'>{auth.data.name}</b>
+                </div>
+                <div className='description event'>{auth.data.event}</div>
               </div>
             </div>
           </div>
-          <div className="logout ui left floated secondary button inverted" onClick={logout}>
-            <i className="left chevron icon"></i>
+          <div className='logout ui left floated secondary button inverted' onClick={logout}>
+            <i className='left chevron icon'></i>
             <span>Log Out</span>
+          </div>
+        </div>
+        <div className='four wide column'>
+          <div className='search ui small icon input '>
+            <input onChange={handleSearch} name='search' type='text' placeholder='Search Game' />
+            <i className='search icon'></i>
           </div>
         </div>
       </section>
 
-      <section className="ui grid">
-          <div className="twelve wide column">
-            <h3 className="ui dividing header">Games</h3>
-            <div className="ui relaxed divided game items links">
-              {games.map(({ name, description, icon, code }) =>
-                <div key={code} className="game item">
-                  {/* <img src={icon} alt="game-icon"></img> */}
-                  <div className="ui small image">
-                    <img src={`../../${icon}`} alt="game-icon" />
+      <section className='ui grid'>
+        <div className='twelve wide column'>
+          <h3 className='ui dividing header'>Games</h3>
+          <div className='ui relaxed divided game items links'>
+            {filteredGames.map(({ name, description, icon, code }) => (
+              <div key={code} className='game item'>
+                <div className='ui small image'>
+                  <img src={`../../${icon}`} alt='game-icon' />
+                </div>
+                <div className='content'>
+                  <div className='header'>
+                    <b className='name'>{name}</b>
                   </div>
-                  <div className="content">
-                    <div className="header"><b className="name">{name}</b></div>
-                    <div className="description">
-                      {description}
-                    </div>
-                    <div className="extra">
-                      <Link to={`/game/${code}`} className="play ui right floated secondary button inverted">
-                        Play
-                        <i className="right chevron icon"></i>
-                      </Link>
-                    </div>
+                  <div className='description'>{description}</div>
+                  <div className='extra'>
+                    <Link to={`/game/${code}`} className='play ui right floated secondary button inverted'>
+                      Play
+                      <i className='right chevron icon'></i>
+                    </Link>
                   </div>
                 </div>
-              )}
-            </div>
+              </div>
+            ))}
           </div>
+        </div>
 
-          <div className="four wide column">
-            <h3 className="ui dividing header">Categories</h3>
-            <div className="ui selection animated list category items">
-             <pre>{ JSON.stringify(categories, null, 2) }</pre>
-            </div>
+        <nav className='four wide column'>
+          <h3 className='ui dividing header'>Categories</h3>
+          <div className='ui selection animated list category items'>
+            {categories.map(({ id, name }) => (
+              <div
+                key={id}
+                className={(id === 1 ? 'clicked ' : '') + `category item`}
+                onClick={() => changeCategories(id)}
+              >
+                <div className='content'>
+                  <div className='header'>{name}</div>
+                </div>
+              </div>
+            ))}
           </div>
-        
+        </nav>
       </section>
 
-      <section>
-        
-      </section>
-      
+      <section></section>
     </div>
   );
 }
