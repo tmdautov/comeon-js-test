4d0b805e33b6ff1c30f373d8c85519528f7a624e
Timur <tmdautov@gmail.com>
2022-05-23 21:48:16 +0600

    fix(): remove unused code

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,37 +1,28 @@
 import { useEffect } from 'react';
-import styles from './Game.css'; 
+import styles from './Game.css';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-
 function Game() {
   let { id } = useParams();
-  console.log(id)
   useEffect(() => {
     window.comeon.game.launch(id);
+  });
 
-    
-    // const { match } = this.props;
-    // console.log(match)
-  })
-  return(
-    <div className="ingame">
-        <div className="ui grid centered">
-          <div className="three wide column">
-            <Link to="/games" className="ui right floated secondary button inverted">
-              <i className="left chevron icon"></i>Back
-            </Link>
-          </div>
-          <div className="ten wide column">
-            <div id="game-launch">
-              {/* {error && (
-                <h2 className="game__not-found">Game not found</h2>
-              )} */}
-            </div>
-          </div>
-          <div className="three wide column"></div>
+  return (
+    <div className='ingame'>
+      <div className='ui grid centered'>
+        <div className='three wide column'>
+          <Link to='/games' className='ui right floated secondary button inverted'>
+            <i className='left chevron icon'></i>Back
+          </Link>
         </div>
+        <div className='ten wide column'>
+          <div id='game-launch'></div>
+        </div>
+        <div className='three wide column'></div>
       </div>
+    </div>
   );
 }
 
